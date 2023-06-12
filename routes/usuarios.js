2933f02b68a93e188e28a1c0b7dfddf690842f7a
Cristian Gallegos <crisisragallegos25@gmail.com>
2023-06-12 15:10:37 -0600

    Cambio

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -10,6 +10,7 @@ const {
     esAdminRole,
     tieneRole
 } = require('../middlewares/index')
+
 const router = Router();
 
 router.get('/', usuariosGet);
@@ -34,7 +35,7 @@ router.delete('/:id',
 [   
     validarJWT,
     //esAdminRole,
-    tieneRole('ADMIN_ROLE', 'VENTAS_ROLE', 'USER_ROLE'),
+    //tieneRole('ADMIN_ROLE', 'VENTAS_ROLE', 'USER_ROLE'),
     check('id', 'No es un id correcto').isMongoId(),
     check('id').custom(existeUsuarioPorId),
     validarCampos
